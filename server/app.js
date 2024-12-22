ccaee3dfa207eafa816b5b5858de202a955aead8
Dinara <ab3825@student.jamk.fi>
2024-12-22 10:45:37 +0200

    client and server are installed, check screenshots

---
+++ server/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Task = require('./models/Task');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/todolist', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Todo API!');
+});
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+});
+
+app.put('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
+    res.json(updatedTask);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.findByIdAndDelete(id);
+    res.status(204).end();
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
