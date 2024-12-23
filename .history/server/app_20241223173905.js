09d3d003dea25fdf5ce5841567417fa72eed08c3
Dinara <ab3825@student.jamk.fi>
2024-12-23 17:39:43 +0200

    Integrate React frontend

--- .history/server/app_20241223173905.js
+++ .history/server/app_20241223173905.js
@@ -7,14 +7,7 @@ const Task = require('./models/Task');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const path = require('path');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
 
 // Middleware
 app.use(cors());
