d052af17ea63696d9cff13f3e09b63aaff079b0d
Dinara <ab3825@student.jamk.fi>
2024-12-25 10:40:25 +0200

    Fix API integration for production

--- .history/server/app_20241225103927.js
+++ .history/server/app_20241225103927.js
@@ -66,7 +66,6 @@ app.patch('/tasks/:id', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
